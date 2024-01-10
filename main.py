6e6c9ae93d2f72706a53b655a13f9282e672c900
Spring <Springsquash93@gmail.com>
2024-01-10 17:19:16 -0500

    Update: Refactor time.sleep delays for enhanced performance.

--- main.py
+++ main.py
@@ -2,6 +2,54 @@ import cv2
 import numpy as np
 from PIL import ImageGrab
 import time
+from config import Config
+import pyautogui
+from autoclicker import AutoClicker
+import threading
+import pygetwindow as gw
+import keyboard
+  
+play = True
+config = Config("config/config.json")
+auto_clicker = AutoClicker(config)
+
+def start_clicker():
+    auto_clicker.start()
+
+def stop_program():
+    print("Stopping program")
+    auto_clicker.stop()
+    stop = False
+    exit(0)
+
+keyboard.add_hotkey('f12', stop_program)
+
+clicker_thread = threading.Thread(target=start_clicker)
+clicker_thread.start()
+
+def no_food():
+    print("No food")
+
+def no_stamina():
+    print("No stamina")
+    auto_clicker.stop()
+    time.sleep(config.regain_stamina.get('pause_duration', 0))
+    print("Regained stamina")
+    auto_clicker.start()
+    
+def no_water():
+    print("No water")
+    slots = config.auto_eat_slots
+    if 'water' in slots:
+        pyautogui.press(str(slots['water']))
+        time.sleep(0.5)
+    if 'food' in slots:
+        pyautogui.press(str(slots['food']))
+        time.sleep(0.5)
+    print("Consumed food & water")
+
+actions = [no_stamina, no_water]
+
 
 def detect_red_in_image(image):
     # Convert BGR to HSV
@@ -32,47 +80,35 @@ def detect_red_in_image(image):
 
     return res, large_contours
 
-def no_food():
-    print("No food")
-
-def no_stamina():
-    print("No stamina")
-
-def no_water():
-    print("No water")
-
-actions = [no_stamina, no_water]
-
-counter = 1
-
 if __name__ == "__main__":
-    # Image capture region
-    right_region = (1850, 120, 1920, 1000)
+    time.sleep(2)
+    screen_width, screen_height = pyautogui.size()
+    
+    window = gw.getWindowsWithTitle("ArkAscended")[0]
+    right_region = (window.left, window.top, window.width, window.height)
 
-    # Load the templates
-    # template1 = cv2.imread('assets/no_food.png')
     template2 = cv2.imread('assets/no_stamina.png')
     template3 = cv2.imread('assets/no_water.png')
     templates = [template2, template3]
 
-    while True:
-        screenshot = ImageGrab.grab(bbox=right_region)
+    while play:
+        screenshot = ImageGrab.grab(right_region)
         screenshot_np = np.array(screenshot)
         screenshot_np = cv2.cvtColor(screenshot_np, cv2.COLOR_RGB2BGR)
 
         red_detected, contours = detect_red_in_image(screenshot_np)
-
+        
         for cnt in contours:
             x, y, w, h = cv2.boundingRect(cnt)
 
-            # Crop contour
+            # Cropped contour
             cropped = red_detected[y:y+h, x:x+w]
 
             for template, action in zip(templates, actions):
                 try:
                     result = cv2.matchTemplate(cropped, template, cv2.TM_CCOEFF_NORMED)
                     _, max_val, _, max_loc = cv2.minMaxLoc(result)
-                    if max_val > 0.9:  # Match threshold
+                    if max_val > 0.9:  # threshold
                         action()
                 except:
                     pass
